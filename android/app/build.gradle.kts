be02ab1830edc59dc522351d9fd5be8950dca471
Kavish Devar <mail@kavishdevar.me>
2024-12-06 21:32:24 +0530

    bump version

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         minSdk = 28
         targetSdk = 35
         versionCode = 1
-        versionName = "0.0.2-beta3"
+        versionName = "0.0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
