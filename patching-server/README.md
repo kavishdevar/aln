40547919cec89cdee9f4de3d8eb3bc1c0701164b
Paul <devnoname120@gmail.com>
2025-01-27 00:38:16 +0100

    Remove legacy patchers

--- patching-server/README.md
+++
@@ -1,7 +0,0 @@
-# Patcher Server
-
-This is the server side of the patcher. It is responsible for serving the patch files to the clients.
-
-# License
-
-Same as project license. Check [/LICENSE](/LICENSE) for more information.
--- root-module-manual/post-data-fs.sh
+++
@@ -1,2 +0,0 @@
-#!/system/bin/sh
-mount -t overlay overlay -o lowerdir=/apex/com.android.btservices/lib64/,upperdir=/data/adb/modules/btl2capfix/apex/com.android.btservices/lib64,workdir=/data/adb/modules/btl2capfix/apex/com.android.btservices/work Zapex/com.android.btservices/lib64
