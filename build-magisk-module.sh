26ca089d302c3b2cc97a2ccfe263119ce4658c6b
Kavish Devar <mail@kavishdevar.me>
2025-02-22 22:25:04 +0530

    CI: add root module zip in nightly release

--- build-magisk-module.sh
+++ build-magisk-module.sh
@@ -2,10 +2,12 @@
 
 set -eux
 
+OUTPUT_ZIP=${1:-../btl2capfix.zip}
+
 cd root-module
-rm -f ../btl2capfix.zip
+rm -f "$OUTPUT_ZIP"
 
 # COPYFILE_DISABLE env is a macOS fix to avoid parasitic files in ZIPs: https://superuser.com/a/260264
 export COPYFILE_DISABLE=1
 
-zip -r ../btl2capfix.zip . -x \*.DS_Store \*__MACOSX \*DEBIAN ._\* .gitignore
+zip -r "$OUTPUT_ZIP" . -x \*.DS_Store \*__MACOSX \*DEBIAN ._\* .gitignore
